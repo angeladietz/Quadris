f804fb3dac29a1293803bb7b4bb88d8b838b95c2
Mayank Kanoria <mayankkanoria@gmail.com>
2020-07-26 23:04:01 -0400

    Initial implementation of tile

--- tile.h
+++ tile.h
@@ -2,16 +2,39 @@
 #define _TILE_
 
 #include <iostream>
+#include <ostream>
 
 class Tile {
     public:
-        Tile();
-        ~Tile();
+        Tile(char);
+        ~Tile() {};
 
         bool isFilled() const;
         char getTileValue() const;
 
         friend std::ostream& operator<< (std::ostream&, Tile&);
+
+    private:
+        char tile_;
+        bool isFilled_ = false;
 };
+
+char Tile::getTileValue() const {
+    return tile_;
+}
+
+bool Tile::isFilled() const {
+    return isFilled_;
+}
+
+Tile::Tile(char tile) {
+   tile_ = tile; 
+}
+
+friend std::ostream& operator<< (std::ostream& out, Tile& t) {
+
+    out << t.getTileValue();
+
+}
 
 #endif
