e014127274a97b60cf9526a5d62123cdab018720
Chandana <csathish@uwaterloo.ca>
2020-07-31 19:30:36 +0530

    Small modification to function definition

--- subject.cc
+++ subject.cc
@@ -4,10 +4,10 @@
 
 #include "observer.h"
 
-void Subject::subscribe(Observer &newObs) { _observers.insert(&newObs); }
+void Subject::subscribe(Observer* newObs) { _observers.insert(newObs); }
 
-void Subject::unsubscribe(Observer &formerView) {
-  _observers.erase(&formerView);
+void Subject::unsubscribe(Observer* formerView) {
+  _observers.erase(formerView);
 }
 
 void Subject::notifyObservers() {
