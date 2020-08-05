6f5be5129141443438d155ef9e1ca60b0f224974
csathish <csathish@student.cs.uwaterloo.ca>
2020-08-05 02:06:45 -0400

    revert gui subscription to board commit

--- GUI.cc
+++ GUI.cc
@@ -21,7 +21,6 @@ GUI::GUI(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& builder, Con
 	controller_ = controller;
 	board_ = board;
 	quadris_ = quadris;
-	board_->subscribe(this);
 
 	_builder->get_widget("objectContainer", objectContainer);
 	_builder->get_widget("scoreBoard", scoreBoard);
